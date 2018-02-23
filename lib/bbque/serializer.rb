988de54f7b24080c3a9c2b41729b8cf12fc2b68e
Benjamin Vetter <vetter@plainpicture.de>
2018-02-23 15:38:26 +0100

    Use base64 in serializer

--- lib/bbque/serializer.rb
+++ lib/bbque/serializer.rb
@@ -4,11 +4,11 @@ require "base64"
 module BBQue
   module Serializer
     def self.dump(object)
-      Marshal.dump(object)
+      Base64.encode64(Marshal.dump(object))
     end
 
     def self.load(object)
-      Marshal.load(object)
+      Marshal.load(Base64.decode64(object))
     end
   end
 end
